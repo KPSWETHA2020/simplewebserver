80433181da048bb7fb8ca9997b815b764ac2c205
swetha <kpswethasweety6767@gmail.com>
2021-02-12 14:05:51 +0000

    mywebserver

--- mywebserver.py
+++ mywebserver.py
@@ -1,23 +1,3 @@
-# Developing a Simple Webserver
-## AIM:
-To develop a simple webserver to serve html pages.
-
-## DESIGN STEPS:
-### Step 1: 
-HTML content creation
-### Step 2:
-Design of webserver workflow
-### Step 3:
-Implementation using Python code
-### Step 4:
-Serving the HTML pages.
-### Step 5:
-Testing the webserver
-
-## PROGRAM:
-
-### mywebserveer.py:
-```
 from http.server import HTTPServer,BaseHTTPRequestHandler
 
 Content="""
@@ -61,17 +41,3 @@ server_address=('',80)
 httpd = HTTPServer(server_address,myhandler)
 print("MY WEBSERVER IS RUNNING...")
 httpd.serve_forever()
-```
-
-
-## OUTPUT:
-
-![output](./static/img/s5.PNG)
-
-
-## CODE VALIDATION REPORT:
-
-![output](./static/img/v1.PNG)
-
-## RESULT:
-Thus a website is designed for a simple webserver to serve html pages and is hosted in the URL : http://swetha.student.saveetha.in/ . HTML code is validated.
